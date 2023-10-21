010d6a08dfc8e9cd73488fc60bd63e10f973a0f9
HannaKaniuk <136392947+HannaKaniuk@users.noreply.github.com>
2023-10-21 09:16:47 +0300

    homework8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,49 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+
+function createGalleryMarkup(items) {
+    return items
+        .map(
+            (item) => `<li class="gallery__item">
+                <a class="gallery__link" href="${item.original}">
+                    <img 
+                        class="gallery__image"
+                        src="${item.preview}"
+                        data-source="${item.original}"
+                        alt="${item.description}"
+                    />
+                </a>
+            </li>`
+        )
+        .join('');
+}
+
+galleryList.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems));
+
+const options = {
+    captionsData: 'alt',
+    captionDelay: 250,
+};
+
+const lightboxRef = new SimpleLightbox('.gallery__link', options);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
+
+const feedbackForm = document.querySelector('.feedback-form');
+const inputEmail = document.querySelector('input[name="email"]');
+const messageTextarea = document.querySelector('textarea[name="message"]');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+
+function saveFormState() {
+  const formState = {
+    email: inputEmail.value,
+    message: messageTextarea.value,
+  };
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formState));
+}
+
+
+function loadFormState() {
+  const savedState = localStorage.getItem(LOCALSTORAGE_KEY);
+  if (savedState) {
+    const formState = JSON.parse(savedState);
+    inputEmail.value = formState.email;
+    messageTextarea.value = formState.message;
+  }
+}
+
+
+loadFormState();
+
+
+inputEmail.addEventListener('input', throttle(saveFormState, 500));
+messageTextarea.addEventListener('input', throttle(saveFormState, 500));
+
+
+feedbackForm.addEventListener('submit', (evt) => {
+  evt.preventDefault(); 
+  console.log('Form data:', {
+    email: inputEmail.value,
+    message: messageTextarea.value,
+  });
+  localStorage.removeItem(LOCALSTORAGE_KEY); 
+});
+
+
+
+
 
